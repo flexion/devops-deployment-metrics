ebc39562437a9247068450a52c2a0bdfdd60607e
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2023-05-18 16:45:56 -0700

    Fixed parameter name for Github constructor

--- src/devops_deployment_metrics/utils.py
+++ src/devops_deployment_metrics/utils.py
@@ -1,5 +1,5 @@
 from github import Github
 
 
-def connect_to_github(username: str, token: str) -> any:
-    return Github(login_or_token=username, token=token, verify=True)
+def connect_to_github(username: str, password: str) -> any:
+    return Github(login_or_token=username, password=password, verify=True)
