4d5e86aff18443afc48404fb6d16c8163f3bff66
Tom Willis <twillis@flexion.us>
2023-06-24 16:27:38 -0500

    Moving the root path calculation to method makes it work in all tests, locally with pytest, and with the nox tests session

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,9 @@
 """Test the utils module."""
+from pathlib import Path
 from typing import Any
 
 from devops_deployment_metrics.utils import connect_to_github
+from devops_deployment_metrics.utils import get_project_root
 
 
 def test_connect_to_github(mocker: Any) -> None:
@@ -12,3 +14,21 @@ def test_connect_to_github(mocker: Any) -> None:
         login_or_token="username", password="token", verify=True
     )
     assert gh == github_mock.return_value
+
+
+def test_get_project_root():
+    # Get the project root using the function
+    project_root = get_project_root()
+
+    # Assert that the returned value is a Path object
+    assert isinstance(project_root, Path)
+
+    # Assert that the returned path exists
+    assert project_root.exists()
+
+    # Assert that the returned path is a directory
+    assert project_root.is_dir()
+
+    # Assert that the returned path is the expected project root
+    expected_root = Path(__file__).parent.parent  # relative to this test file
+    assert project_root == expected_root
