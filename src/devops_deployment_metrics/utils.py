82b96bedeb27b5730ab081f68f8b8b017ef36017
Tom Willis <twillis@flexion.us>
2023-06-25 22:26:05 -0500

    Finish the GitHub Auth fix

--- src/devops_deployment_metrics/utils.py
+++ src/devops_deployment_metrics/utils.py
@@ -8,7 +8,7 @@ from github import Github
 def connect_to_github(username: str, password: str) -> Any:
     """Connect to GitHub."""
     # Create an instance of the Auth.Login class with your GitHub credentials
-    auth = Auth.Login("username", "password")
+    auth = Auth.Login(username, password)
 
     # Create a GitHub instance with the authentication
     return Github(auth=auth, verify=True)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,14 +1,27 @@
 """Test the utils module."""
-from typing import Any
+from unittest.mock import MagicMock
+from unittest.mock import patch
 
 from devops_deployment_metrics.utils import connect_to_github
 
 
-def test_connect_to_github(mocker: Any) -> None:
-    """Test the connect_to_github function."""
-    github_mock = mocker.patch("devops_deployment_metrics.utils.Github")
-    gh = connect_to_github("username", "token")
-    github_mock.assert_called_once_with(
-        login_or_token="username", password="token", verify=True
-    )
-    assert gh == github_mock.return_value
+def test_connect_to_github() -> None:
+    # Mock the necessary objects
+    mock_auth = MagicMock()
+    mock_github = MagicMock()
+
+    # Patch the required classes and methods
+    with patch(
+        "devops_deployment_metrics.utils.Auth.Login", return_value=mock_auth
+    ) as mock_login, patch(
+        "devops_deployment_metrics.utils.Github", return_value=mock_github
+    ) as mock_github_class:
+        username = "test_username"
+        password = "test_password"
+        # Invoke the function
+        gh = connect_to_github(username, password)
+
+        # Assert the expected behavior
+        assert gh == mock_github
+        mock_login.assert_called_once_with(username, password)
+        mock_github_class.assert_called_once_with(auth=mock_auth, verify=True)
