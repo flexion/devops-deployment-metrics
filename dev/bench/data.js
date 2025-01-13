0a8326829e4fd6e0edf84d0442b4717394206690
github-action-benchmark <github@users.noreply.github.com>
2025-01-13 21:44:02 +0000

    add Pytest-Benchmark Benchmark (pytest) benchmark result for 6dfc90f269a404dc3095de98e0aca81c275ea9fe

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1736651781650,
+  "lastUpdate": 1736804642178,
   "repoUrl": "https://github.com/flexion/devops-deployment-metrics",
   "entries": {
     "Pytest-Benchmark Benchmark": [
@@ -50,6 +50,58 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.000019138168954094742",
             "extra": "mean: 274.12436642541405 usec\nrounds: 2901"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "twillis@flexion.us",
+            "name": "Tom Willis",
+            "username": "tomwillis608"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6dfc90f269a404dc3095de98e0aca81c275ea9fe",
+          "message": "Merge pull request #709 from flexion/bug/708/fix-benchmark-ci\n\nUse regular GITHUB_TOKEN, not personal token, to try to  resolve the …",
+          "timestamp": "2025-01-13T15:43:24-06:00",
+          "tree_id": "e05b1bfe985451d6b69384e659f8f1a09114d130",
+          "url": "https://github.com/flexion/devops-deployment-metrics/commit/6dfc90f269a404dc3095de98e0aca81c275ea9fe"
+        },
+        "date": 1736804641162,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_metrics.py::test_deployment_frequency_metric",
+            "value": 1383.4975767307787,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00004379517005995061",
+            "extra": "mean: 722.805747418085 usec\nrounds: 1065"
+          },
+          {
+            "name": "tests/test_metrics.py::test_change_fail_rate_metric",
+            "value": 1394.574096125044,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000056753815618169415",
+            "extra": "mean: 717.0648033536509 usec\nrounds: 1312"
+          },
+          {
+            "name": "tests/test_metrics.py::test_mean_time_to_recovery_metric",
+            "value": 1401.619238348266,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00004695330603451043",
+            "extra": "mean: 713.4605266823015 usec\nrounds: 1293"
+          },
+          {
+            "name": "tests/test_config.py::test_config",
+            "value": 3543.1872126247063,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000014758885409692436",
+            "extra": "mean: 282.2317704345136 usec\nrounds: 2875"
           }
         ]
       }
